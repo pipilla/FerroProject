60b001f0d1bf08637837440a2ee4da08e327132b
pipilla <juanmi-64@hotmail.com>
2025-04-09 12:02:09 +0200

    Creación de base y prueba en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,5 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
-    </div>
+    <x-self.base>
+        <h1 class="text-2xl md:text-3xl font-extrabold text-gray-800 text-center mb-8">WELCOME</h1>
+    </x-self.base>
 </x-app-layout>
