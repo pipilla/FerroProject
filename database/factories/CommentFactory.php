0ed641f70a85bd2ea1b3ba7e2757a7d9251f66fc
pipilla <juanmi-64@hotmail.com>
2025-04-09 15:57:48 +0200

    Factory de comentarios

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,10 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'message' => fake()->text(),
+            'comment_id' => (random_int(0, 2)) ? (Comment::get()->random()->id) : null,
+            'post_id' => Post::get()->random()->id,
+            'user_id' => User::get()->random()->id,
         ];
     }
 }
