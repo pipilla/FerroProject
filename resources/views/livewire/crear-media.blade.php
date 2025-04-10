99633961e4eb31955f06f5961a3d820ec85e58b0
pipilla <juanmi-64@hotmail.com>
2025-04-10 13:55:27 +0200

    Galería

---
+++ resources/views/livewire/crear-media.blade.php
@@ -0,0 +1,62 @@
+<div>
+    <x-button wire:click="set('openModal', true)">
+        <i class="fas fa-add mr-2"></i><span class="font-semibold">Subir contenido</span>
+    </x-button>
+    <x-dialog-modal wire:model="openModal">
+        <x-slot name="title">
+            Subir contenido
+        </x-slot>
+        <x-slot name="content">
+            <!-- Título -->
+            <div class="relative mb-4">
+                <label for="titulo" class="sr-only">Título</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Título del contenido"
+                    wire:model.live="cform.titulo"
+                    class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <i class="fas fa-heading absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+                <x-input-error for="cform.titulo" />
+            </div>
+
+            <!-- Categoría -->
+            <div class="relative">
+                <label for="categoria" class="sr-only">Categoría</label>
+                <select id="categoria" name="categoria" wire:model="cform.category_id"
+                    class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <option value="">Selecciona una categoría</option>
+                    @foreach ($categoria as $category)
+                        <option value="{{ $category->id }}">{{ $category->nombre }}</option>
+                    @endforeach
+                </select>
+                <i class="fas fa-tag absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+                <x-input-error for="cform.category_id" />
+            </div>
+
+            <!-- Media -->
+            <div class="mt-4 w-full relative bg-slate-200 h-80">
+                <input type="file" accept="image/*" id="csrc" class="hidden" wire:loading.attr="disabled"
+                    wire:model="cform.src">
+                <label for="csrc"
+                    class="absolute bottom-2 right-2 bg-green-500 text-white font-bold p-3 rounded-lg hover:bg-green-600 transition duration-300">
+                    <i class="fas fa-upload mr-2"></i>Subir
+                </label>
+                @isset($cform->src)
+                    <img src="{{ $cform->src->temporaryUrl() }}"
+                        class="size-full object-cover object-no-repeat object-center">
+                @endisset
+            </div>
+
+        </x-slot>
+        <x-slot name="footer">
+            <div class="flex flex-row-reverse">
+                <button type="button" wire:click="store" wire:loading.attr="disabled"
+                    class=" bg-blue-500 text-white font-bold p-3 rounded-lg hover:bg-blue-600 transition duration-300">
+                    <i class="fas fa-paper-plane mr-2"></i>Publicar
+                </button>
+                <button type="button" wire:click="cancelar"
+                    class="mr-4 bg-red-500 text-white font-bold p-3 rounded-lg hover:bg-red-600 transition duration-300">
+                    <i class="fas fa-xmark mr-2"></i>Cerrar
+                </button>
+            </div>
+        </x-slot>
+    </x-dialog-modal>
+</div>
--- resources/views/livewire/show-media.blade.php
+++ resources/views/livewire/show-media.blade.php
@@ -1,5 +1,7 @@
 <x-self.base>
 
+    <h1 class="mb-4 text-3xl text-center font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">Galería</span></h1>
+
     <div class="flex items-center justify-center py-4 md:py-8 flex-wrap">
         <button type="button" wire:click="buscar(0)" 
             class="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">Todas las categorías</button>
